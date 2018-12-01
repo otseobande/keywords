c96f6c8daca64fd22e72dba4cb7aa90a0594d6f3
Obande Otse <otseobande@gmail.com>
2018-12-01 23:10:58 +0100

    feat(idf): Implement idf algorithm (#2)

--- tests/test_tf_idf.py
+++ tests/test_tf_idf.py
@@ -1,8 +1,12 @@
 from tf_idf import *
 
 def test_calculate_tf():
-  """calculate_tf function should return the right term frequency"""
+  """The calculate_tf function should return the right term frequency"""
 
-  document = 'The man is the king'
+  assert calculate_tf('the', 'The man is the king') == 0.4
 
-  assert calculate_tf('the', document) == 0.4
+def test_calculate_idf():
+  """The calculate_idf function"""
+
+  assert calculate_idf('food', ['the food is cold', 'I dont want this food']) == 1.0
+  assert calculate_idf('food', ['the food is cold', 'lets go out']) == 2.0
--- tf_idf.py
+++ tf_idf.py
@@ -1,8 +1,12 @@
 def calculate_tf(term, document):
   """Calculates the frequency of a term in a document
 
+  Args:
+    term (str): The term (word) to find the frequency of.
+    document (str): The document to search for the term in.
+
   Returns:
-    float: term frequency
+    float: The term frequency (tf)
   """
 
   term_list = [term.lower() for term in document.split()]
@@ -10,3 +14,20 @@ def calculate_tf(term, document):
   term_count_in_doc = term_list.count(term)
 
   return term_count_in_doc / num_of_words_in_doc
+
+def calculate_idf(term, documents):
+  """Calculates the total number of documents divided by the
+  number of documents containing the term (word)
+
+  Args:
+    term (str): The term (word) to search for
+    documents (list): A list of all the documents to search through
+
+  Returns:
+    float: The Inverse document frequency (idf)
+  """
+
+  num_of_docs = len(documents)
+  documents_containing_term = len([document for document in documents if term in document])
+
+  return num_of_docs / documents_containing_term
